bbc309b1a38ca10ca3a29e913db85453890d87af
Muhammadqodir5 <127942300+Muhammadqodir5@users.noreply.github.com>
2023-05-17 11:09:03 +0500

    react-butstrap boshlangich

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Navbar } from "react-bootstrap";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Butstrap.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Navbar=()=>{
+    return(
+        <>
+        <h1>salom</h1>
+        </>
+    )
+}
+
+export default Navbar;
